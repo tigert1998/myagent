6422116ed583c3e27dc13e8148f681c386b6c2fc
tigertang <tigertang.zju@qq.com>
2026-05-22 14:29:30 +0800

    Fix load_prompt bug

--- myagent/prompt.py
+++ myagent/prompt.py
@@ -6,8 +6,8 @@ import os
 
 
 def load_prompt(path: str, args: dict[str, str]):
-    var_pattern = r"\$\{MYAGENT:([^}]+)\}"
-    file_pattern = r"\$\{MYAGENT_FILE:([^}]+)\}"
+    var_pattern = r"@\{MYAGENT:([^}]+)\}"
+    file_pattern = r"@\{MYAGENT_FILE:([^}]+)\}"
 
     with open(path, "r") as f:
         content = f.read()
@@ -15,7 +15,7 @@ def load_prompt(path: str, args: dict[str, str]):
     for match in re.findall(file_pattern, content):
         filepath = osp.join(osp.dirname(path), match)
         file_content = load_prompt(filepath, args)
-        content = content.replace(f"${{MYAGENT_FILE:{match}}}", file_content)
+        content = content.replace(f"@{{MYAGENT_FILE:{match}}}", file_content)
 
     args = {
         **args,
@@ -26,6 +26,6 @@ def load_prompt(path: str, args: dict[str, str]):
 
     for match in re.findall(var_pattern, content):
         value = args[match.strip()]
-        content = content.replace(f"${{MYAGENT:{match}}}", value)
+        content = content.replace(f"@{{MYAGENT:{match}}}", value)
 
     return content
