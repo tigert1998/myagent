00bf55528b2318656f1ac765a0f8bf228a5c973f
tigertang <tigertang.zju@qq.com>
2026-06-04 03:23:06 +0000

    Add observation mask

--- myagent/agent.py
+++ myagent/agent.py
@@ -1,10 +1,11 @@
+import os
+import os.path as osp
 import traceback
 from typing import Any, Optional, Callable
 import threading
 import json
 
 from myagent.utils import shorten
-from myagent.loggers import Logger
 from myagent.tools.tools_list import ToolsList
 from myagent.llm_client import LLMClient, LLMUsage
 from myagent.prompt import load_prompt
@@ -44,16 +45,54 @@ class ReActAgent(Agent):
         llm_client: LLMClient,
         send_msg: Callable[[str], None],
         tools_list: ToolsList,
-        logger: Logger,
+        log_path: str,
         num_retries: int = 3,
     ) -> None:
         super().__init__(name, llm_client, send_msg, tools_list)
 
-        self.logger: Logger = logger
+        self.log_path = log_path
         self.num_retries = num_retries
 
         self.user_new_msgs_lock = threading.Lock()
         self.user_new_msgs: list[str] = []
+
+    def _mask_tool_results(self, messages: list[dict[str, Any]]) -> None:
+        os.makedirs(f"{self.log_path}/{self.name}/tool_calls", exist_ok=True)
+        last_mask_index = getattr(self, "last_mask_index", 0)
+
+        assistant_indices = [
+            i
+            for i in range(last_mask_index + 1, len(messages))
+            if messages[i]["role"] == "assistant"
+        ]
+        if len(assistant_indices) == 0:
+            return
+
+        # keep latest tool call output in context
+        tool_indices = [
+            i
+            for i in range(last_mask_index + 1, assistant_indices[-1])
+            if messages[i]["role"] == "tool"
+        ]
+        if len(tool_indices) == 0:
+            return
+
+        for i in tool_indices:
+            tool_call_id = messages[i]["tool_call_id"]
+            content = messages[i]["content"]
+            output_path = osp.abspath(
+                f"{self.log_path}/{self.name}/tool_calls/{tool_call_id}.json"
+            )
+            with open(output_path, "w") as f:
+                json.dump(content, f, indent=4, ensure_ascii=False)
+            messages[i]["content"] = f'Tool call output saved at "{output_path}"'
+
+        self.last_mask_index = max(tool_indices)
+
+    def _save_messages(self, messages: list[dict[str, Any]]) -> None:
+        os.makedirs(f"{self.log_path}/{self.name}", exist_ok=True)
+        with open(f"{self.log_path}/{self.name}/messages.json", "w") as f:
+            json.dump(messages, f, indent=4, ensure_ascii=False)
 
     def append_user_new_msg(self, message: str) -> None:
         with self.user_new_msgs_lock:
@@ -70,13 +109,13 @@ class ReActAgent(Agent):
     def _try_one_iter(
         self, messages: list[dict[str, Any]]
     ) -> tuple[list[dict[str, Any]], Optional[str]]:
-        # append new user messages
-        # call llm
-        # tool calls and observation
+        # 1. append new user messages
+        # 2. call llm
+        # 3. invoke tool calls and observation
+        # 4. mask previous tool call outputs
 
         user_new_msgs = self._get_user_new_msgs()
         if len(user_new_msgs) > 0:
-            self.logger.log(self.name, {"user": user_new_msgs})
             messages = messages + [
                 {
                     "role": "user",
@@ -90,8 +129,6 @@ class ReActAgent(Agent):
         tool_calls = response.tool_calls
         self.usage = response.usage
 
-        self.logger.log(self.name, {"think": reasoning_content})
-        self.logger.log(self.name, {"assistant": content})
         if len(content) > 0:
             self.send_msg(content)
         self.try_report_usage()
@@ -122,7 +159,6 @@ class ReActAgent(Agent):
                     name, tool_call["function"]["arguments"]
                 )
                 tool_use_obj = {"tool": name, "args": args}
-                self.logger.log(self.name, {"action": tool_use_obj})
                 tool_use_obj_str = shorten(
                     json.dumps(tool_use_obj, indent=4, ensure_ascii=False), 1024
                 )
@@ -138,7 +174,6 @@ class ReActAgent(Agent):
                 exc_str = traceback.format_exc()
                 observation = [exc_str]
                 msg_to_send = exc_str
-            self.logger.log(self.name, {"observation": observation})
             messages_to_append.append(
                 {
                     "role": "tool",
@@ -153,6 +188,10 @@ class ReActAgent(Agent):
         if len(tool_calls) > 0:
             messages_to_append[0]["tool_calls"] = tool_calls
         messages = messages + messages_to_append
+
+        self._mask_tool_results(messages)
+        self._save_messages(messages)
+
         return messages, None
 
     def _retry_one_iter(
